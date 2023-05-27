869e63656b0e5ac4abbf6eb1e1c670d1c94e328a
daferarte <daferarte@gmail.com>
2023-05-27 14:49:17 -0500

    Arreglos en react

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 
+const nombres=["Daniel","Eduardo","Cody","Pepito"];
+
+function getNombres(){
+  const elementosLista=[];
+  for(let i=0;i<nombres.length;i++){
+    elementosLista.push(<li>{nombres[i]}</li>);
+  }
+
+  console.log(elementosLista);
+  return elementosLista;
+}
+
 function Saludo({idioma}){
     return(
       <div>
         {
           idioma==="es" ? <p>Como estan?</p> : <p>How are you?</p>
-        }        
+        } 
+        <ul>{getNombres()}</ul>       
       </div>
     )
   
