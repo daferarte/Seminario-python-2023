622c6075aa40ec84908d76230427ec30d1e23931
daferarte <daferarte@gmail.com>
2023-05-27 16:45:05 -0500

    agregando rutas en react

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
-import {BrowserRouter, Routes, Route, Link, Outlet, redirect, useNavigate, Navigate} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Link, Outlet, redirect, useNavigate, Navigate, useParams} from 'react-router-dom';
 
 let NotImplemented =()=>{
   return (
     <div>
       <h1>Esta pagina no esta lista</h1>
+      <Link to="/">Ir al inicio</Link>
+    </div>
+  )
+}
+
+let Error404 = () =>{
+  return (
+    <div>
+      <h1>Esta pagina no existe</h1>
       <Link to="/">Ir al inicio</Link>
     </div>
   )
@@ -23,17 +32,30 @@ let ProductosOutlet =()=>{
   )
 }
 
+let ProductosEdit = () =>{
+  let {id} = useParams();
+  let params = useParams();
+  console.log(params);
+
+  return(
+    <p>
+      {id}
+    </p>
+  )
+}
+
 function App() {
-  
+  const isAuth = false;
   return (
     <BrowserRouter>
       <Routes>
         
         <Route path="/" element={<NotImplemented/>}/>
 
-        <Route path="productos" element={<ProductosOutlet/>}>
-          <Route path="add" element={<NotImplemented/>}/>
-          <Route path="edit" element={<NotImplemented/>}/>
+        <Route path="/productos" >
+
+          <Route path="add" element={ isAuth ? <Navigate to='/'/> : <NotImplemented/>}/>
+          <Route path="edit/:id" element={<ProductosEdit/>}/>
           <Route path="delete" element={<NotImplemented/>}/>
         </Route>
 
@@ -43,6 +65,7 @@ function App() {
           <Route path="delete" element={<NotImplemented/>}/>
         </Route>
 
+        <Route path="*" element={<Error404/>}/>
       </Routes>
       
     </BrowserRouter>
