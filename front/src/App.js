12074ff7e9cb2a433d917a8bcca2460ab041026f
daferarte <daferarte@gmail.com>
2023-05-27 14:52:16 -0500

    optimizando funcion con map

--- front/src/App.js
+++ front/src/App.js
@@ -2,23 +2,13 @@ import React from "react";
 
 const nombres=["Daniel","Eduardo","Cody","Pepito"];
 
-function getNombres(){
-  const elementosLista=[];
-  for(let i=0;i<nombres.length;i++){
-    elementosLista.push(<li>{nombres[i]}</li>);
-  }
-
-  console.log(elementosLista);
-  return elementosLista;
-}
 
 function Saludo({idioma}){
     return(
       <div>
         {
           idioma==="es" ? <p>Como estan?</p> : <p>How are you?</p>
-        } 
-        <ul>{getNombres()}</ul>       
+        }    
       </div>
     )
   
@@ -29,6 +19,11 @@ function App() {
     <div >
       <h1>Hola estudiantes</h1>
       <Saludo idioma="es"/>
+      <ul>
+        {
+          nombres.map(nombre => <li>{nombre}</li>)
+        }
+      </ul>
     </div>
   );
 }
